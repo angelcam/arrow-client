86a13ce836be4abab5292f8f18aa5e1cf9b4756b
Ondrej Perutka <ondrej.perutka@angelcam.com>
2016-11-04 11:08:15 +0100

    updated to uuid v0.3

--- src/utils/config.rs
+++ src/utils/config.rs
@@ -1,11 +1,11 @@
 // Copyright 2015 click2stream, Inc.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,7 +24,6 @@ use std::error::Error;
 use std::io::{BufReader, BufWriter, Read, Write};
 use std::fmt::{Display, Formatter};
 
-use utils;
 use net::raw::ether;
 
 use net::arrow::protocol::ScanReport;
@@ -112,9 +111,9 @@ struct JsonConfig<'a> {
 impl<'a> JsonConfig<'a> {
     /// Create a new JsonConfig instance.
     fn new(
-        uuid: String, 
-        passwd: String, 
-        version: usize, 
+        uuid: String,
+        passwd: String,
+        version: usize,
         svc_table: &'a ServiceTable) -> JsonConfig<'a> {
         JsonConfig {
             uuid:      uuid,
@@ -123,26 +122,26 @@ impl<'a> JsonConfig<'a> {
             svc_table: Cow::Borrowed(svc_table)
         }
     }
-    
+
     /// Load configuration from a given file.
     fn load(file: &str) -> Result<JsonConfig<'a>> {
         let mut content = String::new();
         let file        = try!(File::open(file));
         let mut breader = BufReader::new(file);
-        
+
         try!(breader.read_to_string(&mut content));
-        
+
         Ok(try!(json::decode(&content)))
     }
-    
+
     /// Save configuration into a given file.
     fn save(&self, file: &str) -> Result<()> {
         let content     = try!(json::encode(self));
         let file        = try!(File::create(file));
         let mut bwriter = BufWriter::new(file);
-        
+
         try!(bwriter.write(content.as_bytes()));
-        
+
         Ok(())
     }
 }
@@ -174,92 +173,94 @@ impl ArrowConfig {
             svc_table: ServiceTable::new()
         }
     }
-    
+
     /// Get Arrow Client UUID.
     pub fn uuid(&self) -> [u8; 16] {
-        uuid_to_bytes(&self.uuid)
+        self.uuid.as_bytes()
+            .clone()
     }
-    
+
     /// Get formatted Arrow Client UUID.
     pub fn uuid_string(&self) -> String {
-        self.uuid.to_hyphenated_string()
+        format!("{}", self.uuid.hyphenated())
     }
-    
+
     /// Get Arrow Client password.
     pub fn password(&self) -> [u8; 16] {
-        uuid_to_bytes(&self.passwd)
+        self.passwd.as_bytes()
+            .clone()
     }
-    
+
     /// Get current configuration version.
     pub fn version(&self) -> usize {
         self.version
     }
-    
+
     /// Get service according to its ID from the underlaying service table.
     pub fn get(&self, id: u16) -> Option<Service> {
         self.svc_table.get(id)
     }
-    
+
     /// Add a new service into the underlaying service table.
     pub fn add(&mut self, svc: Service) -> Option<u16> {
         self.svc_table.add(svc)
     }
-    
+
     /// Add a new static service (i.e. manually added).
     pub fn add_static(&mut self, svc: Service) -> Option<u16> {
         self.svc_table.add_static(svc)
     }
-    
+
     /// Update active flags of all services.
     pub fn update_active_services(&mut self) -> bool {
         self.svc_table.update_active_services()
     }
-    
+
     /// Get all active services.
     pub fn active_services(&self) -> Vec<Service> {
         self.svc_table.active_services()
     }
-    
+
     /// Increment version of this config.
     pub fn bump_version(&mut self) {
         self.version += 1;
     }
-    
+
     /// Get the underlaying service table.
     pub fn service_table(&self) -> &ServiceTable {
         &self.svc_table
     }
-    
+
     /// Set contents of the service table to a given value.
     pub fn reinit(&mut self, svc_table: ServiceTable) {
         self.svc_table = svc_table
     }
-    
+
     /// Load configuration from a given file.
     pub fn load(file: &str) -> Result<ArrowConfig> {
         let json      = try!(JsonConfig::load(file));
         let uuid      = try!(Uuid::parse_str(&json.uuid));
         let passwd    = try!(Uuid::parse_str(&json.passwd));
         let svc_table = json.svc_table.into_owned();
-        
+
         let res = ArrowConfig {
             uuid:      uuid,
             passwd:    passwd,
             version:   json.version,
             svc_table: svc_table
         };
-        
+
         Ok(res)
     }
-    
+
     /// Save configuration into a given file.
     pub fn save(&self, file: &str) -> Result<()> {
         let json = JsonConfig::new(
-            self.uuid.to_hyphenated_string(),
-            self.passwd.to_hyphenated_string(),
+            format!("{}", self.uuid.hyphenated()),
+            format!("{}", self.passwd.hyphenated()),
             self.version,
             &self.svc_table);
-        
+
         json.save(file)
     }
 }
@@ -267,11 +268,11 @@ impl ArrowConfig {
 impl Display for ArrowConfig {
     fn fmt(&self, f: &mut Formatter) -> result::Result<(), fmt::Error> {
         let json = JsonConfig::new(
-            self.uuid.to_hyphenated_string(),
-            self.passwd.to_hyphenated_string(),
+            format!("{}", self.uuid.hyphenated()),
+            format!("{}", self.passwd.hyphenated()),
             self.version,
             &self.svc_table);
-        
+
         json.fmt(f)
     }
 }
@@ -302,16 +303,4 @@ impl AppContext {
             scan_report:     ScanReport::new()
         }
     }
-}
-
-/// Transform a given UUID into an array of 16 bytes.
-fn uuid_to_bytes(uuid: &Uuid) -> [u8; 16] {
-    let bytes   = uuid.as_bytes();
-    let mut res = [0u8; 16];
-    
-    assert_eq!(bytes.len(), res.len());
-    
-    utils::memcpy(&mut res, bytes);
-    
-    res
 }
