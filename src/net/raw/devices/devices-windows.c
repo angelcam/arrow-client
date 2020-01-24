12736f709853a9ad81c7dfec0568708bef27307f
Ondrej Perutka <ondrej.perutka@angelcam.com>
2020-01-24 11:49:36 +0100

    Minor fixes

--- src/net/raw/devices/devices-windows.c
+++ src/net/raw/devices/devices-windows.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 click2stream, Inc.
+ * Copyright 2020 Angelcam, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -54,7 +54,7 @@ static net_device* get_device_info(const IP_ADAPTER_ADDRESSES* adapter) {
     }
 
     res->name = string_dup(adapter->AdapterName);
-    
+
     memcpy(res->mac_address, adapter->PhysicalAddress, MAC_ADDR_SIZE);
 
     ipv4_address = find_ipv4_unicast_address(adapter);
--- src/net/raw/devices/devices.h
+++ src/net/raw/devices/devices.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 click2stream, Inc.
+ * Copyright 2020 Angelcam, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
